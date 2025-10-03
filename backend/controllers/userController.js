2ae3e1e020744fe406361e3dca048c7e9feb9d2f
Ethan Chou <yqu82888@gmail.com>
2025-10-03 11:00:01 -0400

    added user register controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -24,7 +24,34 @@ const authUser = asyncHandler(async (req, res) => {
 });
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.send("register user");
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    // generate jwt token and set it as http-only cookie (login user)
+    generateToken(res, user._id);
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 });
 
 const logoutUser = asyncHandler(async (req, res) => {
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -30,6 +30,21 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return bcrypt.compare(enteredPassword, this.password);
 };
 
+// Mongoose pre-save middleware for hashing passwords before saving a user to the database
+userSchema.pre("save", async function (next) {
+  // checks if the password field has changed.
+  if (!this.isModified("password")) {
+    // If the password was not modified, we skip hashing and call next() immediately.
+    next();
+  }
+
+  // Hash the password
+  // Generate a salt with cost factor 10.
+  const salt = await bcrypt.genSalt(10);
+  // Hash the password with the salt.
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
 const User = mongoose.model("User", userSchema);
 
 export default User;
